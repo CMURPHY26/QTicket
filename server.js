4b980af46d59d276634b684e4c2c891d4e513dfa
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-03-08 19:57:07 -0400

    changed user index page to display ticket cards and updated header & a tag color

--- server.js
+++ server.js
@@ -90,60 +90,60 @@ app.get("/seed", (req, res) => {
     [
     { 
       resolved:false,
-      name:"New 1",
+      name:"My Laptop Crashed",
       request:"Leggings polaroid four dollar toast banh mi DIY marfa affogato tote bag brooklyn wayfarers yuccie umami aesthetic slow-carb. Chia gastropub gochujang roof party la croix. Pour-over cred small batch woke listicle shoreditch. Street art copper mug poutine lo-fi disrupt swag, hashtag yr bitters kickstarter art party godard bushwick brunch. Art party bespoke vape, succulents stumptown irony quinoa sustainable cold-pressed bushwick cray ennui vexillologist. Hammock literally man braid kickstarter selvage.",
       username:"laurenl"
     },
     {
       resolved:false,
-      name:"New 2",
-      request:"Leggings polaroid four dollar toast banh mi DIY marfa affogato tote bag brooklyn wayfarers yuccie umami aesthetic slow-carb. Chia gastropub gochujang roof party la croix. Pour-over cred small batch woke listicle shoreditch. Street art copper mug poutine lo-fi disrupt swag, hashtag yr bitters kickstarter art party godard bushwick brunch. Art party bespoke vape, succulents stumptown irony quinoa sustainable cold-pressed bushwick cray ennui vexillologist. Hammock literally man braid kickstarter selvage.",
+      name:"Please add this button to the website",
+      request:"Woke sustainable cronut, pitchfork celiac umami fixie butcher gentrify twee brooklyn street art 3 wolf moon franzen. Raw denim fashion axe scenester, mixtape pitchfork air plant cornhole gentrify. Fashion axe tousled cardigan, tilde photo booth poke four loko waistcoat lomo. Skateboard intelligentsia forage, hexagon photo booth iPhone single-origin coffee +1. Shaman DIY meh, farm-to-table enamel pin distillery fingerstache vice williamsburg hoodie organic sartorial hella.",
       username:"laurenl"
     },
     {resolved:false,
-      name:"New 3",
-      request:"Leggings polaroid four dollar toast banh mi DIY marfa affogato tote bag brooklyn wayfarers yuccie umami aesthetic slow-carb. Chia gastropub gochujang roof party la croix. Pour-over cred small batch woke listicle shoreditch. Street art copper mug poutine lo-fi disrupt swag, hashtag yr bitters kickstarter art party godard bushwick brunch. Art party bespoke vape, succulents stumptown irony quinoa sustainable cold-pressed bushwick cray ennui vexillologist. Hammock literally man braid kickstarter selvage.",
+      name:"New Netfork Folder Needed",
+      request:"Neutra austin lumbersexual tilde. Williamsburg pabst franzen pok pok craft beer. Drinking vinegar locavore fashion axe ramps portland meggings bushwick lo-fi edison bulb. Copper mug cornhole PBR&B messenger bag. Trust fund lyft vinyl cliche synth, squid adaptogen fingerstache art party kinfolk blue bottle everyday carry green juice fashion axe.",
       username:"laurenl"
     },
     {
       resolved:false,
-      name:"New 4",
-      request:"Leggings polaroid four dollar toast banh mi DIY marfa affogato tote bag brooklyn wayfarers yuccie umami aesthetic slow-carb. Chia gastropub gochujang roof party la croix. Pour-over cred small batch woke listicle shoreditch. Street art copper mug poutine lo-fi disrupt swag, hashtag yr bitters kickstarter art party godard bushwick brunch. Art party bespoke vape, succulents stumptown irony quinoa sustainable cold-pressed bushwick cray ennui vexillologist. Hammock literally man braid kickstarter selvage.",
+      name:"Website Redesign",
+      request:"Hoodie scenester affogato irony readymade, vegan wolf sartorial umami listicle edison bulb try-hard 90's. Schlitz distillery pitchfork ugh shaman vape pork belly kogi helvetica single-origin coffee. Readymade subway tile brunch pop-up mixtape drinking vinegar 90's selfies lumbersexual. Chicharrones meh pok pok, butcher mustache portland seitan kitsch migas copper mug kombucha. Helvetica adaptogen activated charcoal celiac, next level poke roof party neutra lomo seitan trust fund aesthetic austin selvage crucifix. Brunch poutine chia schlitz listicle banh mi kitsch.",
       username:"laurenl"
     },
     {
       resolved:false,
-      name:"New 5",
-      request:"Leggings polaroid four dollar toast banh mi DIY marfa affogato tote bag brooklyn wayfarers yuccie umami aesthetic slow-carb. Chia gastropub gochujang roof party la croix. Pour-over cred small batch woke listicle shoreditch. Street art copper mug poutine lo-fi disrupt swag, hashtag yr bitters kickstarter art party godard bushwick brunch. Art party bespoke vape, succulents stumptown irony quinoa sustainable cold-pressed bushwick cray ennui vexillologist. Hammock literally man braid kickstarter selvage.",
+      name:"HELP!!!!!!",
+      request:"Asymmetrical yuccie YOLO taxidermy pitchfork direct trade. Photo booth glossier sustainable vexillologist authentic. Heirloom coloring book fingerstache, photo booth seitan ramps tofu gochujang. Kickstarter typewriter williamsburg aesthetic normcore ugh listicle kombucha taiyaki. Ramps actually ethical, letterpress trust fund celiac cardigan food truck 3 wolf moon farm-to-table stumptown gluten-free. Vegan chillwave pabst, yr flexitarian master cleanse iceland heirloom hoodie cold-pressed cloud bread.",
       username:"laurenl"
     },
     {
       resolved:false,
-      name:"New 6",
-      request:"Leggings polaroid four dollar toast banh mi DIY marfa affogato tote bag brooklyn wayfarers yuccie umami aesthetic slow-carb. Chia gastropub gochujang roof party la croix. Pour-over cred small batch woke listicle shoreditch. Street art copper mug poutine lo-fi disrupt swag, hashtag yr bitters kickstarter art party godard bushwick brunch. Art party bespoke vape, succulents stumptown irony quinoa sustainable cold-pressed bushwick cray ennui vexillologist. Hammock literally man braid kickstarter selvage.",
+      name:"Change website ecommerce platform",
+      request:"Taxidermy raw denim kale chips roof party enamel pin normcore VHS XOXO dreamcatcher organic iPhone deep v taiyaki shaman. Before they sold out man braid vaporware, yuccie ramps affogato snackwave tilde. Intelligentsia raclette forage woke, messenger bag deep v subway tile. Health goth iPhone squid tbh small batch knausgaard, palo santo keytar franzen bespoke poutine sriracha readymade cloud bread.",
       username:"laurenl"
     },
     {resolved:false,
-      name:"New 7",
-      request:"Leggings polaroid four dollar toast banh mi DIY marfa affogato tote bag brooklyn wayfarers yuccie umami aesthetic slow-carb. Chia gastropub gochujang roof party la croix. Pour-over cred small batch woke listicle shoreditch. Street art copper mug poutine lo-fi disrupt swag, hashtag yr bitters kickstarter art party godard bushwick brunch. Art party bespoke vape, succulents stumptown irony quinoa sustainable cold-pressed bushwick cray ennui vexillologist. Hammock literally man braid kickstarter selvage.",
+      name:"BLUE SCREEN OF DEATH!!!!",
+      request:"Air plant wolf VHS tumblr 90's hoodie, intelligentsia DIY post-ironic narwhal sartorial mumblecore forage. Sriracha iceland street art shoreditch microdosing, venmo etsy drinking vinegar kitsch polaroid single-origin coffee health goth slow-carb. Mumblecore wayfarers green juice snackwave. Messenger bag portland iPhone four dollar toast echo park, try-hard cred small batch brunch butcher tousled subway tile bitters. Beard plaid woke, tofu pitchfork mixtape viral brunch af banh mi meggings. Bushwick stumptown offal migas, art party flannel selvage. Gochujang quinoa YOLO portland.",
       username:"laurenl"
     },
     {
       resolved:false,
-      name:"New 8",
-      request:"Leggings polaroid four dollar toast banh mi DIY marfa affogato tote bag brooklyn wayfarers yuccie umami aesthetic slow-carb. Chia gastropub gochujang roof party la croix. Pour-over cred small batch woke listicle shoreditch. Street art copper mug poutine lo-fi disrupt swag, hashtag yr bitters kickstarter art party godard bushwick brunch. Art party bespoke vape, succulents stumptown irony quinoa sustainable cold-pressed bushwick cray ennui vexillologist. Hammock literally man braid kickstarter selvage.",
+      name:"OH GOD PLEASE HELP!!!!!",
+      request:"Hexagon hella 3 wolf moon, 8-bit hoodie banh mi street art church-key cronut shaman typewriter pok pok fashion axe. Banh mi locavore brooklyn, tbh aesthetic activated charcoal adaptogen taiyaki viral woke shabby chic live-edge. Typewriter cold-pressed letterpress literally artisan 90's farm-to-table health goth brooklyn skateboard gochujang. Artisan iPhone next level air plant. Locavore hammock air plant gentrify, messenger bag DIY thundercats umami pour-over taiyaki austin literally cliche. Man braid skateboard pour-over, raclette tacos pork belly viral disrupt occupy art party put a bird on it marfa lomo twee. Forage cardigan +1 brooklyn ugh activated charcoal, blog skateboard dreamcatcher bushwick offal chicharrones humblebrag intelligentsia.",
       username:"laurenl"
     },
     {
       resolved:false,
-      name:"New 9",
-      request:"Leggings polaroid four dollar toast banh mi DIY marfa affogato tote bag brooklyn wayfarers yuccie umami aesthetic slow-carb. Chia gastropub gochujang roof party la croix. Pour-over cred small batch woke listicle shoreditch. Street art copper mug poutine lo-fi disrupt swag, hashtag yr bitters kickstarter art party godard bushwick brunch. Art party bespoke vape, succulents stumptown irony quinoa sustainable cold-pressed bushwick cray ennui vexillologist. Hammock literally man braid kickstarter selvage.",
+      name:"Add website plugin",
+      request:"Ethical fashion axe cred, taiyaki gastropub viral bespoke chartreuse salvia enamel pin pug master cleanse. Retro authentic keytar cray pop-up vape church-key williamsburg fam tumeric freegan tacos thundercats. Four dollar toast chillwave street art fashion axe authentic forage, vegan XOXO gluten-free knausgaard. Listicle hell of XOXO offal woke microdosing. Meh umami ethical, cardigan la croix street art master cleanse salvia etsy.",
       username:"laurenl"
     },
     {
       resolved:false,
-      name:"New 10",
-      request:"Leggings polaroid four dollar toast banh mi DIY marfa affogato tote bag brooklyn wayfarers yuccie umami aesthetic slow-carb. Chia gastropub gochujang roof party la croix. Pour-over cred small batch woke listicle shoreditch. Street art copper mug poutine lo-fi disrupt swag, hashtag yr bitters kickstarter art party godard bushwick brunch. Art party bespoke vape, succulents stumptown irony quinoa sustainable cold-pressed bushwick cray ennui vexillologist. Hammock literally man braid kickstarter selvage.",
+      name:"IT TECH ASSISTANCE",
+      request:"Hella palo santo neutra asymmetrical pinterest la croix. Freegan paleo hoodie, tousled asymmetrical hashtag listicle. 3 wolf moon distillery knausgaard tumeric, twee XOXO bicycle rights dreamcatcher brunch sartorial food truck. Polaroid snackwave yr, PBR&B intelligentsia disrupt raclette vice gluten-free sartorial coloring book.",
       username:"laurenl"
     }
     ], (err, createdTickets) =>{
