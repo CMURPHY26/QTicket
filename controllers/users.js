9e4aef898e17ced1d7b946ac2737a967d85354af
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-03-07 19:26:49 -0500

    added semantic ui framework to style app

--- controllers/users.js
+++ controllers/users.js
@@ -5,6 +5,8 @@ const bcrypt = require("bcrypt");
 const User =  require("../models/users.js");
 const Ticket = require("../models/tickets");
 
+
+//NEW User
 router.get('/new', (req, res) => {
     res.render('users/new.ejs', {metaTitle: "Register Your Account", currentUser: req.session.currentUser});
 });
@@ -17,6 +19,7 @@ router.get('/new', (req, res) => {
 //     });
 // });
 
+//INDEX Users
 router.get("/", (req, res) => {
     if(req.session.currentUser) {
         Ticket.find({username: req.session.currentUser.username}, (err, foundTickets) => {
@@ -27,8 +30,9 @@ router.get("/", (req, res) => {
     }
 });
 
-//Hash password storage
+//CREATE User
 router.post("/", (req, res) => {
+    //Hash password storage
     req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
     req.body.username = req.body.username.toLowerCase();
     User.create(req.body, (err, createdUser) => {
